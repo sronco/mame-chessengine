02b4ec963eece4f1a68dfc0ae88c11d2cd6ad79a
Sandro Ronco <sandro.ronco@gmx.com>
2019-07-30 17:42:34 +0200

    Remove duplicate code

--- chessengine/interfaces/supercon.lua
+++ chessengine/interfaces/supercon.lua
@@ -59,48 +59,6 @@ function interface.set_option(name, value)
 	end
 end
 
-function interface.get_options()
-	return { { "spin", "Level", "3", "1", "8"}, }  -- default difficulty level is 3
-end
-
-function interface.set_option(name, value)
-	if (name == "level") then
-		local level = tonumber(value)
-		if (level < 1 or level > 8) then
-			return
-		end
-		emu.wait(1)
-		send_input(":IN.6", 0x200, 1) -- press Level
-		emu.wait(0.5)
-		local curlev = 0
-
-		for i = 0, 7 do
-			if (machine:outputs():get_indexed_value("1.", i) ~= 0) then
-				curlev = curlev + 1
-			end
-		end
-		if curlev == level then
-			send_input(":IN.7", 0x100, 1) -- press Go
-			emu.wait(0.5)
-			return
-		end
-
-		repeat
-			send_input(":IN.6", 0x200, 1) -- press Level
-			curlev = 0
-			for i = 0, 7 do
-				if (machine:outputs():get_indexed_value("1.", i) ~= 0) then
-					curlev = curlev + 1
-				end
-			end
-			emu.wait(0.5)
-		until curlev == level
-
-		send_input(":IN.7", 0x100, 1) -- press Go
-		emu.wait(0.5)
-	end
-end
-
 function interface.get_promotion_led()
 	if     (machine:outputs():get_value("0.1") ~= 0) then	return 'q'
 	elseif (machine:outputs():get_value("0.4") ~= 0) then	return 'r'
